5d5142c4171f5a5a4d299894983ab47a2da73218
cahito <cahito@gmail.com>
2022-11-14 21:50:05 -0300

    Feat - Requisito quatro cumprido.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,10 +33,7 @@ def scrape_novidades(html_content):
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
     try:
-        next_page_link = selector.css(
-            "div.nav-links a.next::attr(href)"
-        ).get()
-        print(next_page_link)
+        next_page_link = selector.css("div.nav-links a.next::attr(href)").get()
         if type(next_page_link) != str:
             raise Exception
     except Exception:
@@ -47,7 +44,30 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    noticia = {}
+    noticia["url"] = selector.css(
+        "head link[rel*=canonical]::attr(href)"
+    ).get()
+    noticia["title"] = (
+        selector.css("section h1.entry-title::text").get().strip()
+    )
+    noticia["timestamp"] = selector.css("section li.meta-date::text").get()
+    noticia["writer"] = selector.css(
+        "section li.meta-author a.url::text"
+    ).get()
+    comments = selector.css(".post-comments").re_first(r"\d+(?= comments)")
+    noticia["comments_count"] = int(comments) if comments else 0
+    topico = selector.css("div.entry-content p")[0].css("*::text").getall()
+    noticia["summary"] = "".join(topico).strip()
+    noticia["tags"] = selector.css(
+        "section.post-tags [rel='tag']::text"
+    ).getall()
+    noticia["category"] = selector.css(
+        "div.meta-category span.label::text"
+    ).get()
+
+    return noticia
 
 
 # Requisito 5

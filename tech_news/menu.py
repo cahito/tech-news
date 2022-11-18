0e61454b349e6c15f54822b890b6865ae9196267
cahito <cahito@gmail.com>
2022-11-18 17:47:25 -0300

    Feat - Requisito doze cumprido.

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,31 @@
+import sys
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    opcoes_de_menu = input(
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por tag;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair."
+    )
+
+    segunda_opcao = {
+        "0": "Digite quantas notícias serão buscadas:",
+        "1": "Digite o título:",
+        "2": "Digite a data no formato aaaa-mm-dd:",
+        "3": "Digite a tag:",
+        "4": "Digite a categoria:",
+    }
+
+    if opcoes_de_menu in ["5", "6", "7"]:
+        print("")
+    elif opcoes_de_menu in segunda_opcao:
+        input(segunda_opcao[opcoes_de_menu])
+    else:
+        sys.stderr.write("Opção inválida\n")

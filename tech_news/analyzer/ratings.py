06a97455e8fbac5e23f12d079712aa0a45154091
cahito <cahito@gmail.com>
2022-11-18 16:53:41 -0300

    Feat - Requisito dez cumprido.

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,20 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = []
+    noticias = find_news()
+    noticias_pop = sorted(
+        noticias, key=lambda order: (-order["comments_count"], order["title"])
+    )
+    for x in range(5):
+        try:
+            result.append((noticias_pop[x]["title"], noticias_pop[x]["url"]))
+        except IndexError:
+            pass
+    
+    return result
 
 
 # Requisito 11
